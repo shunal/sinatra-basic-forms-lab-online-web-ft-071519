83fc3d150db1783d5d48cade920405390aaa8397
Steven Hunal <stevehunal@gmail.com>
2019-09-19 19:56:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ class App < Sinatra::Base
   
   post "/puppy" do
      @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+     @name = @puppy.name
+     @breed = @puppy.breed
+     @age = @puppy.age
     erb :display_puppy
   end   
 end
